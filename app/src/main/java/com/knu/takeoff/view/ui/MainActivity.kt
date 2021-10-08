5e303ad676dec63d626059e2561f77d89bed8fe4
이지우 <ljw7178@jongdali.com>
2021-10-08 18:09:40 +0900

    add: splash screen library mod: navigation mod: icon

--- app/src/main/java/com/knu/takeoff/view/ui/MainActivity.kt
+++ app/src/main/java/com/knu/takeoff/view/ui/MainActivity.kt
@@ -4,25 +4,31 @@ package com.knu.takeoff.view.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.knu.takeoff.R
 import com.knu.takeoff.view.ui.navigation.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_Takeoff)
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MainScreen()
         }
@@ -31,42 +37,75 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val scaffoldState = rememberScaffoldState()
     val navController = rememberNavController()
+    val takeOffRouteScreens = listOf(
+        NavigationItem.Calendar,
+        NavigationItem.Person,
+        NavigationItem.Chat,
+        NavigationItem.Feed
+    )
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
     Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TakeOffTopAppBar(
+                navController = navController,
+                takeOffRouteScreens = takeOffRouteScreens,
+                currentDestination = backStackEntry?.destination,
+            )
+        },
         bottomBar = { BottomNavigationBar(navController) }
-    ) {
-        Navigation(navController)
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Navigation(navController)
+        }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MainScreenPreview(){
-    MainScreen()
+fun TakeOffTopAppBar(
+    navController: NavController,
+    takeOffRouteScreens: List<NavigationItem>,
+    currentDestination: NavDestination?
+) {
+    val currentTakeOffRouteScreen =
+        takeOffRouteScreens.find { takeOffRouteScreen -> takeOffRouteScreen.route == currentDestination?.route }
+
+    if (currentTakeOffRouteScreen != null) {
+        TopAppBar(
+            title = {
+                Text(text = currentTakeOffRouteScreen.title)
+            }
+        )
+    }
 }
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
-    val items = listOf(NavigationItem.Feed, NavigationItem.Calendar, NavigationItem.Chat, NavigationItem.Person)
+    val items = listOf(
+        NavigationItem.Feed,
+        NavigationItem.Calendar,
+        NavigationItem.Chat,
+        NavigationItem.Person
+    )
 
     BottomNavigation(backgroundColor = Color.White, contentColor = Color.Black) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach{ item ->
+        val currentDestination = navBackStackEntry?.destination
+        items.forEach { item ->
             BottomNavigationItem(
                 icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
                 label = { Text(text = item.title) },
                 selectedContentColor = Color.Black,
                 unselectedContentColor = Color.Black.copy(0.4f),
                 alwaysShowLabel = true,
-                selected = currentRoute == item.route,
+                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
                 onClick = {
                     navController.navigate(item.route) {
-
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) {
-                                saveState = true
-                            }
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
                         }
                         launchSingleTop = true
                         restoreState = true
@@ -93,4 +132,10 @@ fun Navigation(navController: NavHostController) {
             PersonScreen(NavigationItem.Person.title)
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
